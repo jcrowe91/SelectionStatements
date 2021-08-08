97ba798ca013fe8f37c20e46047da4a94c12e71d
jcrowe91 <jcrowe9119@gmail.com>
2021-08-07 22:03:15 -0500

    Add project files.

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets play a game! Choose a number 1-100!");
+            int userInput = int.Parse(Console.ReadLine());
+
+            var r = new Random();
+            var favNumber = r.Next(1, 100);
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+
+            Console.WriteLine($"The answer was {favNumber}!");
+
+
+        }
+    }
+}
